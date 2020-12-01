2c788152109e3509ca593df9d262941b892af9ac
ArvindGunjal <arvindgunjal8@gmail.com>
2020-12-01 11:58:33 +0530

    first commit

---
+++ src/main/java/com/heroku/api/HerokuApiApplication.java
@@ -0,0 +1,13 @@
+package com.heroku.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HerokuApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HerokuApiApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/heroku/api/controller/MainController.java
@@ -0,0 +1,25 @@
+package com.heroku.api.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+	
+	@GetMapping("/getMessage")
+	public String getMessageArvind() {
+		
+		return "Hello Arvind";
+		
+	}
+
+	@GetMapping("/")
+	public String getMessage() {
+		
+		return "Hello Arvind";
+		
+	}
+	
+	
+}
